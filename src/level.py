245372a56acb463e0998bdb3d948d1c52756e94f
ThePianist <cafarimo@outlook.com>
2023-05-05 20:09:46 -0400

    Show tool and seed as overlay

--- src/level.py
+++ src/level.py
@@ -1,13 +1,16 @@
 import pygame
 
+from src.overlay import Overlay
 from src.player import Player
 
 
 class Level:
     def __init__(self):
+        self.player = None
         self.display_surface = pygame.display.get_surface()
         self.all_sprites = pygame.sprite.Group()
         self.setup()
+        self.overlay = Overlay(self.player)
 
     def setup(self):
         self.player = Player((640, 360), self.all_sprites)
@@ -16,3 +19,4 @@ class Level:
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
         self.all_sprites.update(dt)
+        self.overlay.display()
